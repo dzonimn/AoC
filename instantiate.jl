60409300924d315610b22019a3b816ae8ebae634
dzonimn <i.m.jonc@gmail.com>
2022-12-19 09:29:48 +0800

    13

--- instantiate.jl
+++ instantiate.jl
@@ -5,14 +5,17 @@ todaydate = day(today())
 print("Enter the file number [$todaydate]: ")
 filename = readline()
 
+filename == "" && (filename = string(todaydate))
 @assert all(isdigit, filename) "Must be integer"
 
 if length(filename) == 1
-    filenamewithzero = "0" * string(filename)
+    jlfilename = "0" * string(filename)
+else
+    jlfilename = filename
 end
 
 filecontents = read("./template.jl", String)
-filecontents = replace(filecontents, "data/" => "data/$filenamewithzero.txt")
+filecontents = replace(filecontents, "data/" => "data/$filename.txt")
 
 write("$filename.jl", filecontents)
-touch("data/$filename.txt")
+touch("data/$jlfilename.txt")
